cb3732d641b49c7f7f53bbcdc7b1a59dd35df0bd
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2017-11-30 15:37:53 +0530

    Adding detail of movie

--- js/main.js
+++ js/main.js
@@ -76,13 +76,32 @@ function getMovie(){
 	   	 //console.log(response);
 	   	 let movie = response.data;
 	   	  let output =`
-	   	    <div class"row">
+	   	    <div class="row">
 	   	       <div class="col-md-4">
-	   	          <img src="${movie.Poster}" >
+	   	          <img src="${movie.Poster}" class="thumbnail" >
 	   	       </div>
 	   	       <div class="col-md-8">
+	   	          <h2 style="margin:25px 0">${movie.Title}</h2>
+	   	            <ul class="list-group">
+		              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+		              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+		              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+		              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+		              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+		              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+		              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+		            </ul>
 	   	       </div>
 	   	    </div>
+	   	    <div class="row">
+	          <div class="movieContent">
+	            <h3>Plot</h3>
+	            ${movie.Plot}
+	            <hr>
+	            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+	            <a href="index.html" class="btn btn-default">Go Back To Search</a>
+	          </div>
+	        </div>
 	   	  `;
 
 	   	  $("#movie").html(output);
